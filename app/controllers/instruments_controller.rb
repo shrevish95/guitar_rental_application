c6788122aca18cb361f962df7f8a3dddce8b1154
Ravi Singh <ravionrails@gmail.com>
2020-03-16 19:05:30 +0530

    added category filter

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -3,6 +3,8 @@ class InstrumentsController < ApplicationController
     if params[:q].present?
       @q = Instrument.ransack(params[:q])
       @instruments = @q.result(distinct: true)
+    elsif params[:category].present?
+      @instruments = Instrument.where(category_id: params[:category])
     else
       @instruments = Instrument.all
     end
